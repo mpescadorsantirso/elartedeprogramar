2bccf57d118ec50344bc25e817c09111a5d5457d
mapescador <miguelangelps@prometeoinnova.com>
2019-01-17 21:02:55 +0100

    Objetos
    Signed-off-by: mapescador <miguelangelps@prometeoinnova.com>

--- Leccion3/objetos.php
+++ Leccion3/objetos.php
@@ -1,28 +1,49 @@
 <?php
 
-class Foo
+interface Auto
 {
-    public $a = "I'm a!";
-    public $b = "I'm b!";
-    public $c;
+    const MOTOR = " tiene motor\n";
 
-    public function getB()
+    public function arrancar();
+}
+trait Personas
+{
+    public function cuantasPersonas()
     {
-        return $this->b;
-    }
-
-    public function setC($c)
-    {
-        $this->c = $c;
-
-        return $this;
-    }
-
-    public function getC()
-    {
-        return $this->c;
+        echo 'El vehiculo tiene capacidad para '.$this->capacidad." personas.\n";
     }
 }
+class Moto implements Auto
+{
+    use Personas;
+    public $ruedas = 2;
+    public $capacidad = 2;
+    public $nombre = 'La moto';
 
-echo (new Foo())->a;      // I'm a!
-echo (new Foo())->getB(); // I'm b!
+    public function arrancar()
+    {
+        echo $this->nombre." arranca\n";
+    }
+
+    public function cuantasRuedas()
+    {
+        echo 'El vehiculo tiene '.$this->ruedas." ruedas.\n";
+    }
+}
+class Coche extends Moto
+{
+    public $ruedas = 4;
+    public $capacidad = 4;
+    public $nombre = 'El coche';
+}
+
+$moto = new Moto();
+echo $moto->nombre.$moto::MOTOR."\n";
+$moto->arrancar();
+$moto->cuantasRuedas();
+$moto->cuantasPersonas();
+$coche = new Coche();
+echo $coche->nombre.$moto::MOTOR."\n";
+$coche->arrancar();
+$coche->cuantasRuedas();
+$coche->cuantasPersonas();
